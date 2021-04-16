7dec90e1a120ef05baafeaed7965e732fba6c6f0
Javier Ayala <franciscojavier20002017@gmail.com>
2021-04-15 22:41:34 -0600

    Clase Gastos

---
+++ src/ClasesAdministracion/GastosEmpresa.java
@@ -0,0 +1,55 @@
+
+package ClasesAdministracion;
+
+import java.util.Date;
+
+public class GastosEmpresa {
+    private String codigoGastos;
+    private Date fecha;
+    private String tipo;
+    private double saldo;
+
+    public GastosEmpresa() {
+    }
+
+    public GastosEmpresa(String codigoGastos, Date fecha, String tipo, double saldo) {
+        this.codigoGastos = codigoGastos;
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+
+    public String getCodigoGastos() {
+        return codigoGastos;
+    }
+
+    public void setCodigoGastos(String codigoGastos) {
+        this.codigoGastos = codigoGastos;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+   
+}
